fad9ae94e70c62f4ca50b0eb360c0a7a313c5023
Christian Jensen <christian@sinch.com>
2020-09-10 20:37:40 -0700

    theme

--- theme.ts
+++ theme.ts
@@ -86,7 +86,7 @@ export const theme = {
     navbar: {
       main: ({ colors }) => colors.primary.main,
       gradient: ({ colors }) => lighten(colors.navbar.main, colors.navbar.main),
-      contrastText: "lightgray",
+      contrastText: "#cccccc",
     },
     button: {
       main: "rgb(255, 198, 88);",
